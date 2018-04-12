331cee28132e73d623ad29e54dd37d6623fd45f9
James E Keenan <jkeenan@cpan.org>
2018-04-12 16:34:10 -0400

    Add version info config file.

---
+++ .version_info.pl
@@ -0,0 +1,8 @@
+%trans = (
+    version => { old => '0.001', new => '0.001' },
+    date    => { old => 'April 12 2018', new => 'April 12 2018' },
+    copyright => { old => '2018', new => '2018' },
+);
+
+1;
+
