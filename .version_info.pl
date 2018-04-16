97917dc77f436e0a0770b8d332cb9df867652908
James E Keenan <jkeenan@cpan.org>
2018-04-16 18:07:37 -0400

    CPAN version 0.002
    Update version number in module.

--- .version_info.pl
+++ .version_info.pl
@@ -1,6 +1,6 @@
 %trans = (
-    version => { old => '0.001', new => '0.001' },
-    date    => { old => 'April 12 2018', new => 'April 12 2018' },
+    version => { old => '0.001', new => '0.002' },
+    date    => { old => 'April 12 2018', new => 'April 16 2018' },
     copyright => { old => '2018', new => '2018' },
 );
 
