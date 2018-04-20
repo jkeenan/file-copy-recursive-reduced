bea330bfa1782f644862d06547d6e6cfd2eec082
James E Keenan <jkeenan@cpan.org>
2018-04-19 21:06:07 -0400

    CPAN version 0.005

--- .version_info.pl
+++ .version_info.pl
@@ -1,6 +1,6 @@
 %trans = (
-    version => { old => '0.003', new => '0.004' },
-    date    => { old => 'April 17 2018', new => 'April 19 2018' },
+    version => { old => '0.004', new => '0.005' },
+    date    => { old => 'April 19 2018', new => 'April 19 2018' },
     copyright => { old => '2018', new => '2018' },
 );
 
--- lib/File/Copy/Recursive/Reduced.pm
+++ lib/File/Copy/Recursive/Reduced.pm
@@ -4,7 +4,7 @@ use warnings;
 
 use parent qw( Exporter );
 our @EXPORT_OK = qw( dircopy fcopy rcopy );
-our $VERSION = '0.004';
+our $VERSION = '0.005';
 
 use File::Copy;
 use File::Find;
