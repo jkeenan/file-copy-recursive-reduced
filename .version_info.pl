cf9dde68b05436b036daa889386c587592d88ca5
James E Keenan <jkeenan@cpan.org>
2018-04-20 10:36:17 -0400

    CPAN version 0.006
    Update README and documentation to reflect Apr 19 2018 issuance of version
    0.41 of File::Copy::Recursive, which obviates the need for further development
    of this library.
    
    Bump $VERSION.

--- .version_info.pl
+++ .version_info.pl
@@ -1,6 +1,6 @@
 %trans = (
-    version => { old => '0.004', new => '0.005' },
-    date    => { old => 'April 19 2018', new => 'April 19 2018' },
+    version => { old => '0.005', new => '0.006' },
+    date    => { old => 'April 19 2018', new => 'April 20 2018' },
     copyright => { old => '2018', new => '2018' },
 );
 
